43ddb5c6ad9150014f0158f6b8a11ce7722440ae
Aryaman Srivastava <aryamansrivastava2006@gmail.com>
2021-02-23 15:51:12 +0530

    Add files via upload

---
+++ untitled0.py
@@ -0,0 +1,10 @@
+from scipy.spatial import distance as dist
+from imutils.video import VideoStream
+from imutils import face_utils
+from threading import Thread
+import numpy as np
+import playsound
+import argparse
+import imutils
+import time
+import cv2
